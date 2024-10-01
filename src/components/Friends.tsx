2578011a97a55266e237b152cb4b89e3615e3024
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-10-01 09:08:22 +0530

    Design Friends page

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -1,10 +1,149 @@
+import { FaIndianRupeeSign } from "react-icons/fa6";
+import { IoGiftSharp } from "react-icons/io5";
+import { LuDog, LuRefreshCw } from "react-icons/lu";
 
 const Friends = () => {
   return (
     <div>
-      Friends
+      <div className="text-center w-full text-white px-2 py-10">
+        <h2 className="text-3xl font-bold">Invite Friends</h2>
+        <p className="text-sm my-2">You and your friend will receive bonuses</p>
+
+        <div className="w-full mt-7 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+          <IoGiftSharp size={55} className="ml-2" />
+          <div className="">
+            <p className="font-semibold text-xl">Invite a friend</p>
+            <div className="flex w-full gap-1 items-center">
+              <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300">
+                <FaIndianRupeeSign size={12} />
+              </div>
+              <span className="text-yellow-500 font-semibold">+5,000</span>
+              for you and your friend
+            </div>
+          </div>
+        </div>
+        <div className="text-left flex items-center mt-10 w-full relative">
+          <p className="font-semibold">List of Your Friends (10)</p>
+          <LuRefreshCw size={24} className="absolute right-2 cursor-pointer" />
+        </div>
+
+        <div className="mt-2 mb-16">
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full my-2 rounded-2xl bg-gray-800 flex items-center gap-x-2 px-1 py-3 text-left">
+            <LuDog size={33} className="ml-2" />
+            <div className="">
+              <p className="font-semibold text-sm">Name</p>
+              <div className="flex w-full gap-1 items-center text-[13px] text-gray-400">
+                <p>Level</p>
+                <div className="rounded-full bg-yellow-500 w-fit h-fit p-0.5 border-[2px] border-yellow-300 text-white">
+                  <FaIndianRupeeSign size={10} />
+                </div>
+                <span className=" font-semibold text-white">100</span>
+                <p>(100)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Friends;
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,12 +3,12 @@ import BottomNav from "../components/BottomBar";
 
 const Layout = () => {
   return (
-    <div className="w-full md:w-[450px] mx-auto text-white h-screen bg-black overflow-hidden">
-      <div className="overflow-scroll">
+    <div className="w-full md:w-[450px] mx-auto text-white min-h-screen bg-black overflow-hidden">
+      <div className="h-[90%]">
         <Outlet />
       </div>
 
-      <div className="fixed bottom-0 w-full left-0">
+      <div className="fixed bottom-0 w-full left-0 h-[10%]">
         <BottomNav />
       </div>
     </div>
