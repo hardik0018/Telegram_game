74775ed9d73c9a4df6795bbe81a725f6a4e85e31
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-10-02 11:30:03 +0530

    remove unused icon

--- src/components/Mine.tsx
+++ src/components/Mine.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, useState } from "react";
+import {  useState } from "react";
 import { CgClose } from "react-icons/cg";
 import { FaIndianRupeeSign } from "react-icons/fa6";
 import { FcGallery } from "react-icons/fc";
