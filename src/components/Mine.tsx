47f8881704fed1393cfd0eccd82e1fded77775fd
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-10-08 09:55:42 +0530

    update card and filter they category

--- src/components/Mine.tsx
+++ src/components/Mine.tsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CgClose } from "react-icons/cg";
 import RuppesCoin from "./RuppesCoin";
 import Coinstatus from "./Coinstatus";
 import Data from "../data/mine";
+import { useContext } from "../context/useContext";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type SingleCard = {
   data: any;
   close?: React.MouseEventHandler;
+  handleUpdate: React.MouseEventHandler;
 };
 const Mine = () => {
+  const { Cards, setCards, setCoin, setPPH, coin } = useContext();
   const Menus = ["Markets", "PR Team", "Tech", "Specials"];
   const [currentMenu, setCurrentMenu] = useState("Markets");
   const [currentCard, setCurrentCard] = useState(false);
@@ -17,6 +22,39 @@ const Mine = () => {
     setCurrentCard(e);
   };
   const hanldeCardClose = () => {
+    setCurrentCard(false);
+  };
+  useEffect(() => {
+    console.log(currentMenu);
+    if (Cards) {
+      let filterdata = Data.filter((item) => {
+        if (item.category == currentMenu) return item;
+      });
+      setCards(filterdata);
+    }
+  }, [currentMenu]);
+
+  const hanldeUpdate = (e: any) => {
+    let cardData: any = [...Cards];
+    cardData.map((item: any) => {
+      const { id, currentlvl, level } = item;
+
+      if (item.id === e) {
+        console.log(id, currentlvl);
+        console.log(coin, level[currentlvl - 1].coin);
+        if (coin >= level[currentlvl - 1].coin) {
+          setCoin((prev) => +prev - level[currentlvl - 1].coin);
+          setPPH((prev) => +prev + level[currentlvl - 1].PPH);
+          console.log(level[currentlvl - 1].coin);
+          item.currentlvl++;
+          toast.success("Success");
+        } else {
+          toast.error("Coin balance is insufficient");
+        }
+      }
+    });
+    console.log(cardData);
+    setCards(cardData);
     setCurrentCard(false);
   };
   return (
@@ -41,7 +79,7 @@ const Mine = () => {
           })}
         </div>
         <div className="w-full text-lg mt-7 my-2 gap-2 mb-24 grid grid-cols-2">
-          {Data.map((item: any) => {
+          {Cards.map((item) => {
             return (
               <div
                 onClick={() => handleSingleCard(item)}
@@ -49,14 +87,14 @@ const Mine = () => {
                 className="rounded-xl bg-gray-700 flex flex-col w-full h-fit"
               >
                 <div className="flex items-center px-1 py-1 gap-3">
-                  <img src={item.img} className="w-1/3 h-16 rounded-md" />
+                  <img src={item.img} className="w-1/3 h-14 rounded-md" />
                   <div className="flex flex-col w-2/3">
                     <h2 className="font-bold">{item.title}</h2>
                     <p className="text-[12px] mt-1">Profit Per hour</p>
                     <div className="flex items-center -mt-2">
                       <RuppesCoin bordersize={2} iconsize={10} />
                       <p className="text-[14px] ml-1 font-semibold">
-                        {item.level[0].PPH}
+                        {item.level[item.currentlvl - 1].PPH}
                       </p>
                     </div>
                   </div>
@@ -64,13 +102,13 @@ const Mine = () => {
                 <div className="w-full mx-auto h-[1px] bg-gray-500 opacity-50"></div>
                 <div className="flex px-2 py-1 items-center gap-3">
                   <p className="w-1/3 text-[15px] font-semibold">
-                    lvl {item.level[0].lvl}
+                    lvl {item.level[item.currentlvl - 1].lvl}
                   </p>
 
                   <div className="flex items-center w-2/3">
                     <RuppesCoin bordersize={2} iconsize={10} />
                     <p className="ml-1 text-[15px] font-semibold">
-                      {item.level[0].coin}
+                      {item.level[item.currentlvl - 1].coin}
                     </p>
                   </div>
                 </div>
@@ -81,14 +119,18 @@ const Mine = () => {
       </div>
       {currentCard && (
         <div className="fixed w-full z-20 h-screen -top-4">
-          <SingleCard data={currentCard} close={hanldeCardClose} />
+          <SingleCard
+            data={currentCard}
+            close={hanldeCardClose}
+            handleUpdate={hanldeUpdate}
+          />
         </div>
       )}
     </section>
   );
 };
 
-const SingleCard = ({ data, close }: SingleCard) => {
+const SingleCard = ({ data, close, handleUpdate }: SingleCard) => {
   return (
     <div className="relative h-full w-full mt-4">
       <div
@@ -106,16 +148,19 @@ const SingleCard = ({ data, close }: SingleCard) => {
           <div className="flex w-full gap-1 items-center justify-center text-[12px] text-gray-400">
             <RuppesCoin bordersize={2} iconsize={8} />
             <span className=" font-semibold text-gray-400">
-              +{data.level[0].PPH}
+              +{data.level[data.currentlvl - 1].PPH}
             </span>
           </div>
           <div className="flex w-full gap-1 items-center justify-center text-[25px] text-gray-400 my-5">
             <RuppesCoin bordersize={2} iconsize={8} />
             <span className="font-semibold text-white">
-              +{data.level[0].coin}
+              +{data.level[data.currentlvl - 1].coin}
             </span>
           </div>
-          <button className="w-[90%] text-xl font-semibold py-5 text-center bg-blue-600 rounded-2xl">
+          <button
+            onClick={() => handleUpdate(data.id)}
+            className="w-[90%] text-xl font-semibold py-5 text-center bg-blue-600 rounded-2xl"
+          >
             Go Ahead
           </button>
           <div
